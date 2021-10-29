ef1d460c02fe60d4c420adf3ff8b084a143109c0
Damien Laffond <damien.laffond@nventive.com>
2021-10-29 13:40:01 -0400

    add configuration builder to register keyvault ( function and web)

--- src/Content/Backend/NV.Templates.Backend.Core/Framework/Configuration/ConfigurationBuilderExtensions.cs
+++ src/Content/Backend/NV.Templates.Backend.Core/Framework/Configuration/ConfigurationBuilderExtensions.cs
@@ -1,4 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using Azure.Core;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 namespace Microsoft.Extensions.Configuration
 {
@@ -20,6 +25,41 @@ namespace Microsoft.Extensions.Configuration
                 reloadOnChange: true);
 
             return config;
+        }
+
+        /// <summary>
+        /// Adds KeyVault as a source of configuration when a configuration key named "KeyVault" is present.
+        /// The "KeyVault" configuration key must contain the KeyVault URL.
+        /// The connection is made using Azure System Managed Identities.
+        /// See https://docs.microsoft.com/en-us/aspnet/core/security/key-vault-configuration?view=aspnetcore-3.0#use-managed-identities-for-azure-resources
+        /// for more information.
+        /// </summary>
+        public static IConfigurationBuilder AddAzureKeyVaultWhenPresent(this IConfigurationBuilder config)
+        {
+            var builtConfig = config.Build();
+            var keyVault = builtConfig["KeyVault"];
+
+            if (!string.IsNullOrWhiteSpace(keyVault))
+            {
+                var tokenCredential = builtConfig.GetTokenCredential();
+
+                var secretClient = new SecretClient(new Uri(keyVault), tokenCredential);
+
+                config.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+            }
+
+            return config;
+        }
+
+        /// <summary>
+        /// Get Token credential with Managed Identity
+        ///  https://docs.microsoft.com/en-us/azure/active-directory/managed-identities-azure-resources/overview.
+        /// </summary>
+        /// <returns>Azure Credential. (Azure = SystemManager identity , VS = signed in account ) ... </returns>
+        public static TokenCredential GetTokenCredential(this IConfiguration builtConfig)
+        {
+            // return new ManagedIdentityCredential(clientId); // Explicit Credential for User Assigned Mananged Identity
+            return new DefaultAzureCredential();
         }
     }
 }
