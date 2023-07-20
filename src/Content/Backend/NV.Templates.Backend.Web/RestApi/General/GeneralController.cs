47644899348f526465410201ca81170596634e11
Alexandra-Maude Grenier <am.grenier@nventive.com>
2023-07-20 13:43:48 -0400

    Add localize test

--- src/Content/Backend/NV.Templates.Backend.Web/RestApi/General/GeneralController.cs
+++ src/Content/Backend/NV.Templates.Backend.Web/RestApi/General/GeneralController.cs
@@ -1,6 +1,10 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NV.Templates.Backend.Core.Framework.Internationalization;
 
 namespace NV.Templates.Backend.Web.RestApi.General
 {
@@ -8,10 +12,12 @@ namespace NV.Templates.Backend.Web.RestApi.General
     public class GeneralController : ControllerBase
     {
         private readonly IApplicationInfo _applicationInfo;
+        private readonly IStringLocalizerEx _localizer;
 
-        public GeneralController(IApplicationInfo applicationInfo)
+        public GeneralController(IApplicationInfo applicationInfo, IStringLocalizerEx localizer)
         {
             _applicationInfo = applicationInfo;
+            _localizer = localizer;
         }
 
         [ApiVersionNeutral]
@@ -22,5 +28,27 @@ namespace NV.Templates.Backend.Web.RestApi.General
         {
             return Ok(new ApplicationInfoModel(_applicationInfo));
         }
+
+#if DEBUG
+        [ApiVersionNeutral]
+        [AllowAnonymous]
+        [HttpGet("localization-test")]
+        [Description("Allows to test localization.")]
+        public ActionResult LocalizationTest(string? key = null)
+        {
+            key ??= "This is a test";
+
+            // Just to ensure there are enough arguments for string.Format.
+            var arguments = Enumerable.Range(0, 100).Select(i => $"{i}").ToArray();
+            return Ok(new Dictionary<string, object>
+            {
+                { "current key", key! },
+                { "request culture", CultureInfo.CurrentUICulture.Name },
+                { "localized value", _localizer[key, arguments] },
+                { "all supported cultures for the given key", _localizer.GetStrings(key, arguments: arguments) },
+                { "all strings of the app in the request culture", _localizer.GetAllStrings(true, arguments) },
+            });
+        }
+#endif
     }
 }
